62362bb015f50b99663ce3b57b7ccc5b889ea22a
Fabrice Jossinet <fjossinet@gmail.com>
2020-05-06 14:06:59 +0200

    Junction knobs improved. Allow junction size modification and rotation of helices at once.

--- src/fr/unistra/rnartist/gui/JunctionKnob.kt
+++ src/fr/unistra/rnartist/gui/JunctionKnob.kt
@@ -5,8 +5,9 @@ import javafx.scene.layout.Pane
 import javafx.scene.layout.VBox
 import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
+import javafx.scene.shape.Polygon
+import java.awt.event.MouseEvent
 import java.awt.geom.Point2D
-
 
 class JunctionKnob(val junctionCircle:JunctionCircle, val mediator:Mediator) : Pane(){
 
@@ -68,6 +69,135 @@ class JunctionKnob(val junctionCircle:JunctionCircle, val mediator:Mediator) : P
             connector.relocate(p.x-10, p.y-10)
             this.getChildren().addAll(connector);
         }
+
+        var up = Polygon()
+        up.fill = Color.LIGHTGRAY
+        up.stroke = Color.DARKGRAY
+        up.getPoints().addAll(arrayOf(
+                75.0-15, 75.0-21,
+                75.0+15, 75.0-21,
+                75.0, 75.0-42))
+        up.setOnMousePressed {
+            up.fill = Color.STEELBLUE
+            junctionCircle.radius = junctionCircle.radius * 1.1
+            junctionCircle.layout = junctionCircle.layout //a trick to recompute the stuff
+            this.mediator.canvas2D.secondaryStructureDrawing.get().computeResidues(junctionCircle)
+            this.mediator.canvas2D.repaint()
+        }
+        up.setOnMouseReleased {
+            up.fill = Color.LIGHTGRAY
+        }
+        this.getChildren().addAll(up);
+
+        var bottom = Polygon()
+        bottom.fill = Color.LIGHTGRAY
+        bottom.stroke = Color.DARKGRAY
+        bottom.getPoints().addAll(arrayOf(
+                75.0-15, 75.0+21,
+                75.0+15, 75.0+21,
+                75.0, 75.0+42))
+        bottom.setOnMousePressed {
+            bottom.fill = Color.STEELBLUE
+            junctionCircle.radius = junctionCircle.radius * 0.9
+            junctionCircle.layout = junctionCircle.layout //a trick to recompute the stuff
+            this.mediator.canvas2D.secondaryStructureDrawing.get().computeResidues(junctionCircle)
+            this.mediator.canvas2D.repaint()
+        }
+        bottom.setOnMouseReleased {
+            bottom.fill = Color.LIGHTGRAY
+        }
+        this.getChildren().addAll(bottom);
+
+        var left = Polygon()
+        left.fill = Color.LIGHTGRAY
+        left.stroke = Color.DARKGRAY
+        left.getPoints().addAll(arrayOf(
+                75.0-21, 75.0-15,
+                75.0-21, 75.0+15,
+                75.0-42, 75.0))
+        left.setOnMousePressed {
+            left.fill = Color.STEELBLUE
+            //first we search the connector inID (the red circle
+            var inIDIndex:Int = 0
+            for (connector in this.connectors) {
+                if (connector.isInId) {
+                    inIDIndex = this.connectors.indexOf(connector)
+                    break
+                }
+            }
+            if (this.connectors[(inIDIndex+1)%16].selected) //we do nothing, a selected circle is just on the left
+                it.consume()
+            else {
+                var currentPos = (inIDIndex+1)%16
+                while (currentPos != inIDIndex) {
+                    if (!connectors[currentPos].isInId && connectors[currentPos].selected) {
+                        connectors[currentPos].selected = false
+                        connectors[currentPos].fill = Color.LIGHTGRAY
+                        connectors[if (currentPos-1 == -1) 15 else currentPos-1].selected = true
+                        connectors[if (currentPos-1 == -1) 15 else currentPos-1].fill = Color.STEELBLUE
+                    }
+                    currentPos = (currentPos+1)%16
+                }
+                junctionCircle.layout = this.getJunctionLayout().toMutableList()
+                this.mediator.canvas2D.secondaryStructureDrawing.get().computeResidues(junctionCircle)
+                //we need to update the other knobs since the modification of this layout could have produced impacts on other junctions
+                mediator.toolbox.junctionKnobs.children.forEach {
+                    var junctionKnob  = ((it as VBox).children.first() as JunctionKnob)
+                    if (junctionKnob != this)
+                        junctionKnob.loadJunctionLayout()
+                }
+            }
+        }
+        left.setOnMouseReleased {
+            left.fill = Color.LIGHTGRAY
+        }
+        this.getChildren().addAll(left);
+
+        var right = Polygon()
+        right.fill = Color.LIGHTGRAY
+        right.stroke = Color.DARKGRAY
+        right.getPoints().addAll(arrayOf(
+                75.0+21, 75.0-15,
+                75.0+21, 75.0+15,
+                75.0+42, 75.0))
+        right.setOnMousePressed {
+            right.fill = Color.STEELBLUE
+            //first we search the connector inID (the red circle)
+            var inIDIndex:Int = 0
+            for (connector in this.connectors) {
+                if (connector.isInId) {
+                    inIDIndex = this.connectors.indexOf(connector)
+                    break
+                }
+            }
+            if (this.connectors[if (inIDIndex-1 == -1) 15 else inIDIndex-1].selected) //we do nothing, a selected circle is just on the right
+                it.consume()
+            else {
+                var currentPos = if (inIDIndex-1 == -1) 15 else inIDIndex-1
+                while (currentPos != inIDIndex) {
+                    if (!connectors[currentPos].isInId && connectors[currentPos].selected) {
+                        connectors[currentPos].selected = false
+                        connectors[currentPos].fill = Color.LIGHTGRAY
+                        connectors[(currentPos+1)%16].selected = true
+                        connectors[(currentPos+1)%16].fill = Color.STEELBLUE
+                    }
+                    currentPos = if (currentPos-1 == -1) 15 else currentPos-1
+                }
+                junctionCircle.layout = this.getJunctionLayout().toMutableList()
+                this.mediator.canvas2D.secondaryStructureDrawing.get().computeResidues(junctionCircle)
+                //we need to update the other knobs since the modification of this layout could have produced impacts on other junctions
+                mediator.toolbox.junctionKnobs.children.forEach {
+                    var junctionKnob  = ((it as VBox).children.first() as JunctionKnob)
+                    if (junctionKnob != this)
+                        junctionKnob.loadJunctionLayout()
+                }
+            }
+        }
+        right.setOnMouseReleased {
+            right.fill = Color.LIGHTGRAY
+        }
+        this.getChildren().addAll(right);
+
         this.loadJunctionLayout()
     }
 
