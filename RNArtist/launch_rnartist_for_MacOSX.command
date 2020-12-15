2a22aa4f26a2ab1c544f6735d91073d8bcc9ab18
fjossinet <fjossinet@gmail.com>
2020-12-15 20:53:33 +0100

    Startup files modified to remove the JAVA_HOME variable. The user needs to have the java command already configured.

--- RNArtist/launch_rnartist_for_MacOSX.command
+++ RNArtist/launch_rnartist_for_MacOSX.command
@@ -1,6 +1,4 @@
 #!/bin/bash
-#set here your the location of your Java platform
-JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-14.jdk/Contents/Home"
 
 cd "`dirname "$0"`"
-$JAVA_HOME/bin/java -Xms500M -Xmx500M --module-path lib --add-modules=javafx.controls,javafx.swing,javafx.web --add-exports javafx.graphics/com.sun.javafx.css=ALL-UNNAMED --add-opens javafx.controls/javafx.scene.control.skin=ALL-UNNAMED -jar lib/rnartist-1.0.jar
+java -Xms500M -Xmx500M --module-path lib --add-modules=javafx.controls,javafx.swing,javafx.web --add-exports javafx.graphics/com.sun.javafx.css=ALL-UNNAMED --add-opens javafx.controls/javafx.scene.control.skin=ALL-UNNAMED -jar lib/rnartist-1.0.jar
--- RNArtist/launch_rnartist_for_Windows.ps1
+++ RNArtist/launch_rnartist_for_Windows.ps1
@@ -1,7 +1,4 @@
 #This is a powershell cript. To be able to launch it, you should need to type first: Set-ExecutionPolicy Unrestricted
 
-#set here your the location of your Java platform
-$JAVA_HOME="C:\Program Files\Java\jdk-13.0.1\"
-
 cd $PSScriptRoot\lib
-Start-Process -FilePath $JAVA_HOME\bin\java.exe -NoNewWindow -Wait -ArgumentList @("-Xms500M -Xmx500M --module-path lib --add-modules=javafx.controls,javafx.swing,javafx.web --add-exports javafx.graphics/com.sun.javafx.css=ALL-UNNAMED --add-opens javafx.controls/javafx.scene.control.skin=ALL-UNNAMED -jar lib\rnartist-1.0.jar")
+Start-Process -FilePath java.exe -NoNewWindow -Wait -ArgumentList @("-Xms500M -Xmx500M --module-path lib --add-modules=javafx.controls,javafx.swing,javafx.web --add-exports javafx.graphics/com.sun.javafx.css=ALL-UNNAMED --add-opens javafx.controls/javafx.scene.control.skin=ALL-UNNAMED -jar lib\rnartist-1.0.jar")
