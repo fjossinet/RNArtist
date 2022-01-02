e2c9df9f8d037ab9ebbe597bc0bb8f91605160e8
Fabrice Jossinet <fjossinet@gmail.com>
2022-01-02 19:08:16 +0100

    In the script editor, when the bracket notation element is added, it recovers the 2D displayed (if any) and its sequence.

--- src/main/kotlin/io/github/fjossinet/rnartist/model/editor/BracketNotationKw.kt
+++ src/main/kotlin/io/github/fjossinet/rnartist/model/editor/BracketNotationKw.kt
@@ -8,9 +8,21 @@ class BracketNotationKw(val parent:SecondaryStructureInputKw?, script: Script, i
         super.addToFinalScript(add)
         if (add) {
             //this.parent?.disableAddbuttons(true)
-            val bnParameter = DSLParameter(script, StringWithoutQuotes(script,"value"), Operator(script,"="), StringValueWithQuotes(script,"click me to set your structure", true), this.indentLevel+1)
+            var bracketNotation = "click me to set your structure"
+            script.mediator.drawingDisplayed.get()?.let {
+                bracketNotation = it.drawing.secondaryStructure.toBracketNotation()
+            }
+            val bnParameter = DSLParameter(script, StringWithoutQuotes(script,"value"), Operator(script,"="), StringValueWithQuotes(script, bracketNotation, true), this.indentLevel+1)
             this.children.add(1, bnParameter)
-            this.children.add(1, SequenceBnParameter(script, bnParameter, this.indentLevel+1))
+
+            var sequence = "click me to set your sequence"
+            var inFinalScript = false
+            script.mediator.drawingDisplayed.get()?.let {
+                sequence = it.drawing.secondaryStructure.rna.seq
+                inFinalScript = true
+            }
+            val seq = SequenceBnParameter(script, bnParameter, this.indentLevel+1, inFinalScript, sequence)
+            this.children.add(1, seq)
             this.children.add(1, OptionalDSLParameter(script, null, StringWithoutQuotes(script,"name"), Operator(script,"="), StringValueWithQuotes(script,"A", true), this.indentLevel+1))
             this.children.add(BracketNotationKw(parent, script, indentLevel))
         } else {
--- src/main/kotlin/io/github/fjossinet/rnartist/model/editor/SequenceBnParameter.kt
+++ src/main/kotlin/io/github/fjossinet/rnartist/model/editor/SequenceBnParameter.kt
@@ -4,24 +4,40 @@ import io.github.fjossinet.rnartist.core.model.RNA
 import io.github.fjossinet.rnartist.core.model.SecondaryStructure
 import io.github.fjossinet.rnartist.gui.editor.Script
 
-class SequenceBnParameter(script: Script, val bnValue:DSLParameter, indentLevel:Int, inFinalScript:Boolean = false): OptionalDSLParameter(script, null, StringWithoutQuotes(script,"seq"), Operator(script,"="), StringValueWithQuotes(script,"", true), indentLevel, inFinalScript, false) {
+class SequenceBnParameter(script: Script, val bnValue: DSLParameter, indentLevel: Int, inFinalScript: Boolean = false, sequence:String = "click me to set your sequence") :
+    OptionalDSLParameter(
+        script,
+        null,
+        StringWithoutQuotes(script, "seq"),
+        Operator(script, "="),
+        StringValueWithQuotes(script, sequence, true),
+        indentLevel,
+        inFinalScript,
+        false
+    ) {
 
-    override fun addToFinalScript(add:Boolean) {
+    override fun addToFinalScript(add: Boolean) {
         super.addToFinalScript(add)
         if (add) {
-            (children.get(2) as? ParameterField)?.let { sequenceFieldValue ->
-                if (sequenceFieldValue.text.text.replace("\"","").trim().isEmpty()) {
-                    (bnValue.children.get(2) as? ParameterField)?.let { bnFieldValue ->
-                        val bn = bnFieldValue.text.text.replace("\"", "")
+            var sequence = "click me to set your sequence"
+            script.mediator.drawingDisplayed.get()?.let {
+                sequence = it.drawing.secondaryStructure.rna.seq
+            } ?: run {
+                (bnValue.children.get(2) as? ParameterField)?.let { bnFieldValue ->
+                    val bn = bnFieldValue.text.text.replace("\"", "")
+                    if (bn.matches(Regex("^[\\.\\(\\)\\{\\}\\[\\]A-Za-z\\-]+$"))) {
                         val rna = RNA(
                             "fake",
                             (1..bn.length).map { listOf("A", "U", "G", "C").random() }.joinToString(separator = "")
                         )
                         val ss = SecondaryStructure(rna, bracketNotation = bn)
                         ss.randomizeSeq()
-                        sequenceFieldValue.text.text = "\"${ss.rna.seq}\""
+                        sequence = ss.rna.seq
                     }
                 }
+            }
+            (children.get(2) as? ParameterField)?.let { sequenceFieldValue ->
+                sequenceFieldValue.text.text = "\"${sequence}\""
             }
         }
 
