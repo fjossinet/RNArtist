ee186bd3d45c73f1452089e052b80d37a47a0f42
Fabrice Jossinet <fjossinet@gmail.com>
2022-01-29 10:41:47 +0100

    Fixed: the launch scripts

--- RNArtist/launch_rnartist_for_Linux.sh
+++ RNArtist/launch_rnartist_for_Linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "`dirname "$0"`"
-java -Xms500M -Xmx500M --module-path lib --add-modules=javafx.controls,javafx.swing,javafx.web --add-exports javafx.graphics/com.sun.javafx.css=ALL-UNNAMED --add-opens javafx.controls/javafx.scene.control.skin=ALL-UNNAMED -jar lib/rnartist-1.0.jar
+java -Xms500M -Xmx500M --module-path lib --add-modules=javafx.controls,javafx.swing-jar lib/rnartist-1.0.jar
--- RNArtist/launch_rnartist_for_Windows.ps1
+++ RNArtist/launch_rnartist_for_Windows.ps1
@@ -1,4 +1,4 @@
 #This is a powershell script. To be able to launch it, you should need to type first: Set-ExecutionPolicy Unrestricted
 
 cd $PSScriptRoot
-Start-Process -FilePath java.exe -NoNewWindow -Wait -ArgumentList @("-Xms500M -Xmx500M --module-path lib --add-modules=javafx.controls,javafx.swing,javafx.web --add-exports javafx.graphics/com.sun.javafx.css=ALL-UNNAMED --add-opens javafx.controls/javafx.scene.control.skin=ALL-UNNAMED -jar lib\rnartist-1.0.jar")
+Start-Process -FilePath java.exe -NoNewWindow -Wait -ArgumentList @("-Xms500M -Xmx500M --module-path lib --add-modules=javafx.controls,javafx.swing -jar lib\rnartist-1.0.jar")
